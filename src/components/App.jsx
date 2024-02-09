19587b9734fd5f97f8b4abb5e40558980f8de9f4
Serg1853 <sergey24226@gmail.com>
2024-02-09 19:32:17 +0200

    del comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,35 +51,3 @@ export const App = () => {
   );
 };
 
-// import { useSelector } from 'react-redux';
-// import { Container } from './App.styled';
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { ContactsList } from './ContactList/ContactList';
-// import { Filter } from './Filter/Filter';
-// import {
-//   getError,
-//   getIsLoading,
-//   getPhoneBookValue,
-// } from 'redux/contacts/contactsSlice';
-// import { Loader } from './Loader/Loader';
-// import { Error } from './Error/Error';
-
-// export const App = () => {
-//   const isLoading = useSelector(getIsLoading);
-//   const error = useSelector(getError);
-//   const phoneBook = useSelector(getPhoneBookValue);
-//   return (
-//     <Container>
-//       <h1>Phonebook</h1>
-//       <ContactForm />
-//       <h2>Contacts</h2>
-//       {phoneBook.lenght === 0 && !error && !isLoading ? (
-//         "You don't have any contacts yet"
-//       ) : (
-//         <Filter />
-//       )}
-//       {isLoading && <Loader />}
-//       {error ? <Error /> : <ContactsList />}
-//     </Container>
-//   );
-// };
